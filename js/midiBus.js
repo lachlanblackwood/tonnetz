16d64fbc1f67e847856cb130b3c5079a4a06d7f7
Corentin Guichaoua <corentin.guichaoua@gmail.com>
2024-06-14 18:45:32 +0200

    hidden connect_out function can now takes a device (partial) name

--- js/midiBus.js
+++ js/midiBus.js
@@ -13,11 +13,23 @@ var midiBus=new Vue({
     }
 });
 
-function connect_out(){
-    out = JZZ().openMidiOut()
-    midiBus.connect(out)
-    // midiBus.midiThru.disconnect(proto.synth)
-    proto.synth.close()
+function connect_out(name){
+    let out = undefined;
+    for (outdevice of JZZ().info().inputs){
+        if(outdevice.name.includes(name)){
+            console.log(`Found device matching ${name}`)
+            out = JZZ().openMidiOut(outdevice)
+            break;
+        }
+    }
+    if(name === undefined){
+        out = JZZ().openMidiOut()
+    }
+    if(out !== undefined){
+        midiBus.connect(out)
+        // midiBus.midiThru.disconnect(proto.synth)
+        proto.synth.close()
+    }
 }
 
 var Tonnetz_midiBus = true;
